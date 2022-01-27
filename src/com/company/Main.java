510aeab464f250ee4bd3d6c2bd304d22f46f07a5
Sorbon Imomnazarov <isorbon@gmail.com>
2022-01-27 03:17:58 +0200

    here is your car now dont ask me more

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
 //        the basic structure
         CarBasics car = new CarBasics();
-        car.color = "red";
-        car.engine = "7999cc";
-        car.door = "2";
-        car.number = "291659";
+
+        car.color = "green";
+        car.number = "654ss";
+        car.engine = "65656cc";
+        car.door = "3";
 
         System.out.println(car.color);
 
